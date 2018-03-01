1069284cee386cfac3da957b40174d2f20fe19ae
Kiyunna <flouriusk3@gator.uhd.edu>
2018-03-01 08:57:46 -0600

    Learning Node.js
    Node.js Practice

---
+++ Traversy/app.js
@@ -0,0 +1,25 @@
+/* Creating a simple web server using Node.js*/
+
+/* Declare variables*/
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+fs.readFile('index.html', (err, html) => {
+	if(err){
+		throw err;
+	}
+	const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-type', 'text/html');
+	res.write(html);
+	res.end('Hello World!');
+	});
+
+	server.listen(port, hostname, () => {
+		console.log('Server started on port ' + port);
+	});
+});
+
